603f7f540615719e2367ce182829b9247c730048
Andy <andythedeveloper0@gmail.com>
2021-01-26 19:43:56 +0900

    Update wiringpi.c

--- c/wiringpi.c
+++ c/wiringpi.c
@@ -9,20 +9,15 @@
 
 int main ()
 {
-    int fd;
-    char c;
-    char buf[100];
-    int cnt, i, cnt1;
-
-    
+    char c, buf[100];
+    int gfd, cnt, i, cnt1, start;    
     int LEN = 300;
-    char Key[LEN];
+    char key[LEN];
     char *start_pt;
-
-    int start;
     int comma[5];
+
     // Setup serial port on ODROID
-    if ((fd = serialOpen ("/dev/ttyACM0",9600)) < 0) {
+    if ((gfd = serialOpen ("/dev/ttyACM0",9600)) < 0) {
         fprintf (stderr, "Unable to open serial device: %s\n", strerror (errno)) ;
         return 1 ;
     }
@@ -31,56 +26,46 @@ int main ()
         return 1 ;
     }
 
-    //serialPrintf(fd,"\r"); // send enter key to read data from sensor
+    //serialPrintf(gfd,"\r"); // send enter key to read data from sensor
     delay(1000);
 
     
 
-    while (1)//(serialDataAvail (fd)) 
+    while (1)//(serialDataAvail (gfd)) 
     {
-        //c = serialGetchar(fd);
-        //printf("%c",c);
 
         start = 0;
         for(i=0;i<LEN;i++)
         {
-            Key[i] = serialGetchar(fd);
+            key[i] = serialGetchar(gfd);
 
-            if((i<LEN-68)&&(i>5)&&(Key[i-5]=='$')&&(Key[i-1]=='G')&&(Key[i]=='A'))
+            if((i<LEN-68)&&(i>5)&&(key[i-5]=='$')&&(key[i-1]=='G')&&(key[i]=='A'))
             {
                 start = i-5;
-                *start_pt = Key[start];
-                //printf("%c %c %c %c %c\n",Key[start+1],Key[start+2],Key[start+3],Key[start+4], Key[start+5]);
-                
-
-               // printf("%c %c %c %c %c %c %c %c %c %c \n",Key[start+17],Key[start+18],Key[start+19],Key[start+20], Key[start+21], 
-                //Key[start+22], Key[start+23],Key[start+24],Key[start+25],Key[start+26]);
-            }
+                *start_pt = key[start];
+                }
             
         }
-        //printf("%c \n",*start_pt);
-        //printf("%c %c %c %c %c\n",Key[start+1],Key[start+2],Key[start+3],Key[start+4], Key[start+5]);
-
-
+        
         if(start!=0)
         {
-            //longitude
-            printf("%c %c %c %c %c",Key[start+17],Key[start+18],Key[start+19],Key[start+20], Key[start+21]);
-            printf("%c %c %c %c %c \n",Key[start+22], Key[start+23],Key[start+24],Key[start+25],Key[start+26]);
-
-            //latitude
-            printf("%c %c %c %c %c",Key[start+30],Key[start+31],Key[start+32],Key[start+33], Key[start+34]);
-            printf("%c %c %c %c %c %c \n",Key[start+35], Key[start+36],Key[start+37],Key[start+38],Key[start+39],Key[start+40]);
-            //printf("%c %c %c %c %c %c %c %c %c %c \n",Key[start+17],Key[start+18],Key[start+19],Key[start+20], Key[start+21], Key[start+22], Key[start+23],Key[start+24],Key[start+25],Key[start+26]);
+            //latitude (ddmm.mmmmm)
+            // from key[start+17] to key[start+26]
+            printf("\n");
+            for (i=17; i<27; i++){
+                printf("%c", key[start+i]);
+            }         
+            
+            //longitude (ddmm.mmmmm)
+            // from key[start+30] to key[start+40]
+            printf("\n");
+            for (i=30; i<41; i++){
+                printf("%c", key[start+i]);
+            }
         }
         
-        
-        delay(300);
-
-
-        
-        
+        delay(300);        
     }
 
-    serialClose(fd);    
+    serialClose(gfd);    
 }
